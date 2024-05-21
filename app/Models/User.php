4dde7f2d526da7b6e14a52967305b344f1fc5a12
saepulfariz <saepulfariz108@gmail.com>
2024-05-21 20:44:11 +0700

    add migrate and seed members

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,7 +27,6 @@ class User extends Authenticatable
         'telephone',
         'address',
         'birthday',
-        'coordinator'
     ];
 
     /**
---
+++ database/seeders/MemberSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class MemberSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                'divisi_id' => 1,
+                'user_id' => 1,
+                'coordinator' => 1,
+            ],
+            [
+                'divisi_id' => 2,
+                'user_id' => 2,
+                'coordinator' => 1,
+            ],
+            [
+                'divisi_id' => 3,
+                'user_id' => 3,
+                'coordinator' => 1,
+            ],
+        ];
+        DB::table('members')->insert($data);
+    }
+}
