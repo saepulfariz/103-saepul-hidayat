19f02aa1823804e8a2b1dba725bbcb2e8d688cba
saepulfariz <saepulfariz108@gmail.com>
2024-05-30 20:28:02 +0700

    fix width table class

--- resources/views/pages/divisions/index.blade.php
+++ resources/views/pages/divisions/index.blade.php
@@ -19,7 +19,7 @@
                 </div> --}}
                 <div class="card-body pb-2">
                     <div class="table-responsive p-0">
-                        <table class="table align-items-center mb-0">
+                        <table class="table align-items-center w-100 mb-0">
                             <thead>
                                 <tr>
                                     <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">No
