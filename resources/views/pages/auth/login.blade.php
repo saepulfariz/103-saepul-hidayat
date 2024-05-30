061265a14268c6aa91088d7b182b6594467aaac6
saepulfariz <saepulfariz108@gmail.com>
2024-05-30 07:08:09 +0700

    remove sign up login page

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -49,13 +49,6 @@
                                         </div>
                                     </form>
                                 </div>
-                                <div class="card-footer text-center pt-0 px-lg-2 px-1">
-                                    <p class="mb-4 text-sm mx-auto">
-                                        Don't have an account?
-                                        <a href="javascript:;" class="text-primary text-gradient font-weight-bold">Sign
-                                            up</a>
-                                    </p>
-                                </div>
                             </div>
                         </div>
                         <div
